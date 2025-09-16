ea2569f10863e324b8483b816687d47ca77f3a7d
Seu Nome <fernandestick.v0@gmail.com>
2025-09-16 00:23:17 -0300

    Esqueletos

--- app/filter.tsx
+++ app/filter.tsx
@@ -1,7 +1,17 @@
-//
-//  filter.tsx
-//  
-//
-//  Created by Jose Fernandes de Andrade Barbosa Lira on 16/09/25.
-//
+import { Link } from 'expo-router';
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
+export default function FilterScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Tela de Perfil</Text> {/* Mude para Tela de Filtro */}
+      <Link href="/home" style={styles.link}>Voltar para Home</Link>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 24, fontWeight: 'bold' },
+  link: { marginTop: 20, color: '#007bff', fontSize: 16 },
+});
