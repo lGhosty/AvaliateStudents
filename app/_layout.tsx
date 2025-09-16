217c380a8b02ba125533c8c656b80437b8aae311
Seu Nome <fernandestick.v0@gmail.com>
2025-09-16 00:17:36 -0300

    Layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
-//
-//  _layout.tsx
-//  
-//
-//  Created by Jose Fernandes de Andrade Barbosa Lira on 16/09/25.
-//
+import { Stack } from 'expo-router';
+import React from 'react';
 
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="home" />
+      <Stack.Screen name="register" />
+      <Stack.Screen name="details" />
+      <Stack.Screen name="profile" />
+      <Stack.Screen name="filter" />
+    </Stack>
+  );
+}
