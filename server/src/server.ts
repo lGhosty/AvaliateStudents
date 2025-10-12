f67bf7d939dcc51a89216278fba59af05b687072
Seu Nome <fernandestick.v0@gmail.com>
2025-10-12 01:33:11 -0300

    Finalização do back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,16 @@
-//
-//  server.ts
-//  
-//
-//  Created by Jose Fernandes de Andrade Barbosa Lira on 11/10/25.
-//
+import express from 'express';
+import cors from 'cors';
+import { userRoutes } from './routes/userRoutes';
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/users', userRoutes);
+
+const PORT = 3333;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
